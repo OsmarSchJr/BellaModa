ab56cd49f4bcbee563e023b0505ec37a4bda7393
OsmarSchJr <osmarschleijunior@hotmail.com>
2023-10-31 12:16:00 -0300

    Criação Migrate users e address

---
+++ Backend/src/database/migrations/20231031141830-users.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+
+      reset_password_token: {
+        type: Sequelize.STRING,
+      },
+
+      reset_password_expires: {
+        type: Sequelize.DATE,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+  
+    await queryInterface.dropTable('users');
+     
+  }
+};
---
+++ Backend/src/database/migrations/20231031143337-addresses.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.createTable('addresses', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      neighborhood: {
+        type: Sequelize.STRING,
+      },
+
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.dropTable('addresses');
+    
+  }
+};
